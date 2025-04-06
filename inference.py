afb57a68bd69e1321a36031acb7da8ccd3a8f878
Jorge Mendez <jorgmen@gmail.com>
2025-04-05 20:34:04 -0600

    Construyendo motor de inferencia con pytholog

--- inference.py
+++ inference.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
-filename = "geo.txt"
+filename = "kb_files/geo.txt"
 
 dataset_path = os.path.join(script_directory, filename)
 
@@ -16,4 +16,5 @@ ex = pl.KnowledgeBase("geo")
 
 ex.from_file(dataset_path)
 
+print(ex.query(pl.Expr("is_canton_of(X, san_jose)")))
 

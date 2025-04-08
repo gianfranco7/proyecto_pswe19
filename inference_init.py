fbdf98b275b228d4500820fd4a44aa450bd668d0
Jorge Mendez <jorgmen@gmail.com>
2025-04-07 22:26:05 -0600

    Agregada regla para inferir peligrosidad de un canton

--- inference_init.py
+++ inference_init.py
@@ -2,53 +2,37 @@ import os
 import csv
 import pytholog as pl
 import pandas as pd
+import inference_utils as iu
 from inference import InferenceEngine 
 
-
 # Usa la clase InferenceEngine como privada
-__ie__ = InferenceEngine()
-
 __script_directory__ = os.path.dirname(os.path.abspath(__file__))
 
 __geo_kb__ = "kb_files\geo.txt"
 
 __incidencias_path__ = "datasets\incidencia.csv"
 
-# Creo que no sera necesaria esta funcion
-# def load_kb_from_file(engine: InferenceEngine, path_to_file: str) -> InferenceEngine:
-#     try:
-#         engine.knowledgeBase.from_file(path_to_file)
-#     except ValueError as e:
-#         print(f"Error! load_csv: {e}")
-
-def load_csv_as_dataframe(path) -> pd.DataFrame:
-    try:
-        df = pd.read_csv(path)
-        return df
-    except ValueError as e:
-        print(f"Error! load_csv: {e}")
-
-
-__incidencia_df__ = load_csv_as_dataframe(os.path.join(__script_directory__, __incidencias_path__))
-
-
-def df_column_to_facts(df: pd.DataFrame, column: str, fact_name: str, engine: InferenceEngine):
-    try:
-        unique_values = df[column].unique()
-        fact_name = fact_name.lower().replace(" ", "_")
-        for val in unique_values:
-            val = val.lower().replace(" ", "_")
-            engine.knowledgeBase([f"{fact_name}({val})"])
-    except ValueError as e:
-        print(f"Error! load_csv: {e}")
-
-
-
-df_column_to_facts(__incidencia_df__, "Incidencia", "incidence_level", __ie__)
+__ie__ = InferenceEngine()
 
 __ie__.knowledgeBase.from_file(os.path.join(__script_directory__, __geo_kb__))
 
+__incidencia_df__ = iu.load_csv_as_dataframe(os.path.join(__script_directory__, __incidencias_path__))
+
+iu.df_column_to_facts(__incidencia_df__, "Incidencia", "incidence_level", __ie__)
+
+
+def __asociate_canton_to_incidence_level__():
+    #agregar el hecho para cada canton
+    for i in range(__incidencia_df__.shape[0]):
+        canton = (f"{__incidencia_df__.Canton[i]}").lower().replace(" ", "_")
+        incidence_level = (f"{__incidencia_df__.Incidencia[i]}").lower().replace(" ", "_")
+        __ie__.knowledgeBase([f"canton_incidence_level({canton}, {incidence_level})"])
     
+    #agregar la regla para determinar peligrosidad de un canton
+    __ie__.knowledgeBase([f"is_dangerous(C, Truth) :- canton(C), canton_incidence_level(C, L) Truth is L == 'muy_alto' or L == 'alto'"])
+
+
+__asociate_canton_to_incidence_level__()
 
 #__ie__.print_kb_db()
-#print(__ie__.knowledgeBase.query(pl.Expr("is_canton_of(escazu, san_jose)")))
+print(__ie__.knowledgeBase.query(pl.Expr("is_dangerous(escazu, R)")))
---
+++ inference_utils.py
@@ -0,0 +1,27 @@
+#inference_utils module
+
+import pandas as pd
+from inference import InferenceEngine
+
+
+
+def df_column_to_facts(df: pd.DataFrame, column: str, fact_name: str, engine: InferenceEngine):
+    try:
+        unique_values = df[column].unique()
+        fact_name = fact_name.lower().replace(" ", "_")
+        for val in unique_values:
+            val = val.lower().replace(" ", "_")
+            engine.knowledgeBase([f"{fact_name}({val})"])
+    except ValueError as e:
+        print(f"Error! load_csv: {e}")
+
+
+
+def load_csv_as_dataframe(path) -> pd.DataFrame:
+    try:
+        df = pd.read_csv(path)
+        return df
+    except ValueError as e:
+        print(f"Error! load_csv: {e}")
+
+
